2d579321554588073e08b17df67449b288f4aa49
Ren Yoshida <ren.yoshida1992@gmail.com>
2022-12-11 17:28:37 +0900

    init.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void hello_world();
+
+using namespace std;
+
+int main() {
+    hello_world();
+    return 0;
+}
+
+// hello worldの標準出力処理
+void hello_world() {
+    cout << "Hello, World!" << endl;
+}
